8cdc9fdc09faa4797634dabc12813c9db3a39536
pramepree <pramepree159@gmail.com>
2024-01-03 13:43:14 +0700

    Example1 in php
    Example1 in php

---
+++ a.php
@@ -0,0 +1,7 @@
+<?php $a = 4;
+$b = 10;
+echo "Result 1 = " . ($a + $b), "<br>";
+echo "Result 1 = " . ($b - $a), "<br>";
+echo "Result 1 = " . ($a * $b), "<br>";
+echo "Result 1 = " . ($b / $a), "<br>";
+?>
